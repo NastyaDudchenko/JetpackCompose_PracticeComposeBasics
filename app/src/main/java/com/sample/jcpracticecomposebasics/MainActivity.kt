d66d6727eb76d0643a416fdfa58a80b4f2f18f11
anastasiia <anastasiia.dudchenko@onix-systems.com>
2023-06-14 15:55:07 +0300

    Created Compose article

--- app/src/main/java/com/sample/jcpracticecomposebasics/MainActivity.kt
+++ app/src/main/java/com/sample/jcpracticecomposebasics/MainActivity.kt
@@ -3,13 +3,23 @@ package com.sample.jcpracticecomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sample.jcpracticecomposebasics.ui.theme.JCPracticeComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JCPracticeComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticle()
                 }
             }
         }
@@ -30,17 +39,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ComposeArticle() {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        ComposeText(
+            title = stringResource(R.string.article_title),
+            paragraph1 = stringResource(R.string.article_paragraph1),
+            paragraph2 = stringResource(R.string.article_paragraph2)
+        )
+    }
+}
+
+@Composable
+fun ComposeText(
+    title: String,
+    paragraph1: String,
+    paragraph2: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = paragraph1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = paragraph2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JCPracticeComposeBasicsTheme {
-        Greeting("Android")
+        ComposeArticle()
     }
 }
