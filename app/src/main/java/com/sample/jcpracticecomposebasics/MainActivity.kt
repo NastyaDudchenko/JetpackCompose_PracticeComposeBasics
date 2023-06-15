c67c64ca3b0d20913270af76b51815a3891cf939
anastasiia <anastasiia.dudchenko@onix-systems.com>
2023-06-15 15:05:02 +0300

    Created Compose quadrant

--- app/src/main/java/com/sample/jcpracticecomposebasics/MainActivity.kt
+++ app/src/main/java/com/sample/jcpracticecomposebasics/MainActivity.kt
@@ -3,13 +3,25 @@ package com.sample.jcpracticecomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sample.jcpracticecomposebasics.ui.theme.JCPracticeComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +29,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JCPracticeComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeQuadrant()
                 }
             }
         }
@@ -30,17 +41,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ComposeQuadrant() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            InfoCard(
+                title = stringResource(R.string.info_title_text),
+                description = stringResource(R.string.info_description_text),
+                background = Color(color = 0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                title = stringResource(R.string.info_title_image),
+                description = stringResource(R.string.info_description_image),
+                background = Color(color = 0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            InfoCard(
+                title = stringResource(R.string.info_title_row),
+                description = stringResource(R.string.info_description_row),
+                background = Color(color = 0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                title = stringResource(R.string.info_title_column),
+                description = stringResource(R.string.info_description_column),
+                background = Color(color = 0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoCard(
+    title: String,
+    description: String,
+    background: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(background)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JCPracticeComposeBasicsTheme {
-        Greeting("Android")
+        ComposeQuadrant()
     }
 }
